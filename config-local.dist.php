a9b72ac3bec2e7f7a6ea2b70d9242347ab16951b
Mistralys <s.mordziol@gmail.com>
2023-06-21 09:06:27 +0200

    Added new configuration constants.

--- config-local.dist.php
+++ config-local.dist.php
@@ -1,7 +1,26 @@
 <?php
 
-    // The password that can be used to connect to the site.
-    define('APP_PASSWORD', '');
+declare(strict_types=1);
+
+namespace Mistralys\SeriesManager;
+
+/**
+ * The password that can be used to connect to the site.
+ */
+const APP_PASSWORD = '';
     
-    // Salt for hashing the password in the session. This can be any arbitrary string.
-    define('APP_SALT', '');
+/**
+ * Salt for hashing the password in the session. This can be any arbitrary string.
+ */
+const APP_SALT = '';
+
+/**
+ * The subscriber PIN, created with a subscription to
+ * TheTVDB API service.
+ */
+const APP_SUBSCRIBER_PIN = '';
+
+/**
+ * The API key, also created with a subscription.
+ */
+const APP_API_KEY = '';
