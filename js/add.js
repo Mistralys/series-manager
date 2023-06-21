b8dffe1035f2ca1a3421462dd03925848c658a4b
Mistralys <s.mordziol@gmail.com>
2023-06-21 09:23:14 +0200

    Updated the generated links.

--- js/add.js
+++ js/add.js
@@ -1,17 +1,23 @@
 function UpdateSearchLinks()
 {
-	var urls = {
+	const urls = {
 		'IMDB':'https://www.imdb.com/find?ref_=nv_sr_fn&q={NAME}&s=all',
-		'TV.com':'http://www.tv.com/search?q={NAME}',
-		'TVDB':'https://www.thetvdb.com/?string={NAME}&tab=listseries&function=Search'
+		'TVDB':'https://www.thetvdb.com/search?query={NAME}'
 	};
 	
-	var name = $('#f-name').val().trim();
-	var links = [];
-	$.each(urls, function(label, url) {
-		url = url.replace('{NAME}', name);
-		links.push('<a href="'+url+'">'+label+'</a>');
-	});
-	
-	$('#searchlinks').html('Search for name in: '+links.join(' | '));
+	const name = $('#f-name').val().trim();
+	let html = '';
+
+	if(name.length > 1)
+	{
+		let links = [];
+		$.each(urls, function(label, url) {
+			url = url.replace('{NAME}', name);
+			links.push('<a href="'+url+'" target="_blank">'+label+'</a>');
+		});
+
+		html = 'Search for name in: '+links.join(' | ');
+	}
+
+	$('#searchlinks').html(html);
 }
