52caa056085d5431d5f5b9888f53a1967e23f056
Mistralys <s.mordziol@gmail.com>
2023-06-21 09:03:40 +0200

    Added types.

--- assets/classes/Bootstrap3Renderer.php
+++ assets/classes/Bootstrap3Renderer.php
@@ -1,144 +1,150 @@
 <?php
 
+declare(strict_types=1);
+
+namespace Mistralys\SeriesManager;
+
+use AppUtils\OutputBuffering;
+use HTML_QuickForm2_Container_Group;
+use HTML_QuickForm2_Element_Button;
+use HTML_QuickForm2_Element_InputCheckbox;
+use HTML_QuickForm2_Element_InputRadio;
+use HTML_QuickForm2_Node;
+use HTML_QuickForm2_Renderer;
+
 class Bootstrap3Renderer extends HTML_QuickForm2_Renderer
 {
-   /**
-    * @var array
-    */
-    protected $hiddens = array();
-    
-   /**
-    * @var string[]
-    */
-    protected $contents = array();
-    
-   /**
-    * @var string
-    */
-    protected $formStart = '';
-    
-   /**
-    * @var string
-    */
-    protected $formEnd = ''; 
-    
-   /**
-    * @var HTML_QuickForm2_Element_Button
-    */
-    protected $buttons = array();
-    
-    public function renderHidden(HTML_QuickForm2_Node $element)
+    /**
+     * @var string[]
+     */
+    protected array $hiddens = array();
+
+    /**
+     * @var string[]
+     */
+    protected array $contents = array();
+
+    protected string $formStart = '';
+
+    protected string $formEnd = '';
+
+    /**
+     * @var HTML_QuickForm2_Element_Button[]
+     */
+    protected array $buttons = array();
+
+    public function renderHidden(HTML_QuickForm2_Node $element) : void
     {
-        $this->hiddens[] = '<input '.$element->getAttributes(true).'>';
+        $this->hiddens[] = '<input ' . $element->getAttributes(true) . '>';
     }
 
-    public function renderElement(HTML_QuickForm2_Node $element)
+    public function renderElement(HTML_QuickForm2_Node $element) : void
     {
-        ob_start();
-        
+        OutputBuffering::start();
+
         $errorClass = '';
-        
-        if($element->hasErrors())
+
+        if ($element->hasErrors())
         {
             $errorClass = 'has-error';
         }
-        
-        if($element instanceof HTML_QuickForm2_Element_InputCheckbox)
+
+        if ($element instanceof HTML_QuickForm2_Element_InputCheckbox)
         {
             ?>
-            	<div class="checkbox <?php echo $errorClass ?>">
-                    <label>
-	                    <?php echo (string)$element ?> <?php echo $element->getLabel() ?>
-                    </label>
-                    <p class="help-block"><?php echo $element->getComment() ?></p>
-                </div>
-            <?php 
+            <div class="checkbox <?php echo $errorClass ?>">
+                <label>
+                    <?php echo $element ?><?php echo $element->getLabel() ?>
+                </label>
+                <p class="help-block"><?php echo $element->getComment() ?></p>
+            </div>
+            <?php
         }
-        if($element instanceof HTML_QuickForm2_Element_InputRadio)
+        if ($element instanceof HTML_QuickForm2_Element_InputRadio)
         {
             ?>
-                <div class="radio <?php echo $errorClass ?>">
-                    <label>
-	                    <?php echo (string)$element ?>
-                    	<?php echo $element->getLabel() ?>
-                    </label>
-                    <p class="help-block"><?php echo $element->getComment() ?></p>
-                </div>
-            <?php 
+            <div class="radio <?php echo $errorClass ?>">
+                <label>
+                    <?php echo $element ?>
+                    <?php echo $element->getLabel() ?>
+                </label>
+                <p class="help-block"><?php echo $element->getComment() ?></p>
+            </div>
+            <?php
         }
-        if($element instanceof HTML_QuickForm2_Element_Button)
+        if ($element instanceof HTML_QuickForm2_Element_Button)
         {
             $this->buttons[] = $element;
         }
         else
         {
             ?>
-            	<div class="form-group <?php echo $errorClass ?>">
-            		<label for="<?php echo $element->getId() ?>"><?php echo $element->getLabel() ?></label>
-            		<?php echo (string)$element ?>
-            		<p class="help-block"><?php echo $element->getComment() ?></p>
-            	</div>
-        	<?php
+            <div class="form-group <?php echo $errorClass ?>">
+                <label for="<?php echo $element->getId() ?>"><?php echo $element->getLabel() ?></label>
+                <?php echo $element ?>
+                <p class="help-block"><?php echo $element->getComment() ?></p>
+            </div>
+            <?php
         }
-    	
-    	$this->contents[] = ob_get_clean();
+
+        $this->contents[] = OutputBuffering::get();
     }
 
-    public function startGroup(HTML_QuickForm2_Node $group)
+    public function startGroup(HTML_QuickForm2_Container_Group $group) : void
     {
-        
+
     }
 
-    public function finishGroup(HTML_QuickForm2_Node $group)
+    public function finishGroup(HTML_QuickForm2_Container_Group $group) : void
     {
-        
+
     }
 
     public function reset()
     {
-        
+
     }
 
-    public function startForm(HTML_QuickForm2_Node $form)
+    public function startForm(HTML_QuickForm2_Node $form) : void
     {
-        $this->formStart = '<form '.$form->getAttributes(true).'>';
+        $this->formStart = '<form ' . $form->getAttributes(true) . '>';
     }
 
-    public function finishForm(HTML_QuickForm2_Node $form)
+    public function finishForm(HTML_QuickForm2_Node $form) : void
     {
         $this->formEnd = '</form>';
     }
 
-    public function startContainer(HTML_QuickForm2_Node $container)
+    public function startContainer(HTML_QuickForm2_Node $container) : void
     {
-        
+
     }
 
-    public function finishContainer(HTML_QuickForm2_Node $container)
+    public function finishContainer(HTML_QuickForm2_Node $container) : void
     {
-        
+
     }
-    
+
     public function __toString()
     {
-        ob_start();
-        
+        OutputBuffering::start();
+
         ?>
-        	<?php echo $this->formStart ?>
-        	
-        	<div class="hiddens">
-        		<?php echo implode(PHP_EOL, $this->hiddens) ?>
-        	</div>
-        	<div class="form-elements">
-        		<?php echo implode(PHP_EOL, $this->contents) ?>
-        	</div>
-        	<div class="form-buttons">
-        		<?php echo implode(PHP_EOL, $this->buttons) ?>
-        	</div>
-        	
-        	<?php echo $this->formEnd ?>
-        <?php 
-        
-        return ob_get_clean();
+        <?php echo $this->formStart ?>
+
+        <div class="hiddens">
+            <?php echo implode(PHP_EOL, $this->hiddens) ?>
+        </div>
+        <div class="form-elements">
+            <?php echo implode(PHP_EOL, $this->contents) ?>
+        </div>
+        <div class="form-buttons">
+            <?php echo implode(PHP_EOL, $this->buttons) ?>
+        </div>
+
+        <?php echo $this->formEnd ?>
+        <?php
+
+        return OutputBuffering::get();
     }
 }
