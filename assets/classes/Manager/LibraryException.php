2b3d0db7685d5944100716d94d245dfca461d0ff
Mistralys <s.mordziol@gmail.com>
2023-06-21 17:02:47 +0200

    Added exception flavors.

---
+++ assets/classes/Manager/LibraryException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\SeriesManager\Manager;
+
+use Mistralys\SeriesManager\ManagerException;
+
+class LibraryException extends ManagerException
+{
+
+}
---
+++ assets/classes/ManagerException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mistralys\SeriesManager;
+
+use AppUtils\BaseException;
+
+class ManagerException extends BaseException
+{
+
+}
