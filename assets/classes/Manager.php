2139b6aee7fa1c266e380854cd6426eecfec5fee
Mistralys <s.mordziol@gmail.com>
2023-06-21 09:05:06 +0200

    Added `createClient()` for the TVDB API access.

--- assets/classes/Manager.php
+++ assets/classes/Manager.php
@@ -2,31 +2,37 @@
 
 declare(strict_types=1);
 
+namespace Mistralys\SeriesManager;
+
+use Adrenth\Thetvdb\Client;
+use Mistralys\SeriesManager\Series\Series;
+use Mistralys\SeriesManager\SeriesCollection;
+
 class Manager
 {
-   /**
-    * @var Series
-    */
+    /**
+     * @var SeriesCollection
+     */
     protected $series;
-    
-   /**
-    * @var string
-    */
+
+    /**
+     * @var string
+     */
     protected $page = 'list';
 
-   /**
-    * @var Manager
-    */
+    /**
+     * @var Manager
+     */
     protected static $instance;
-    
-   /**
-    * @var boolean
-    */
+
+    /**
+     * @var boolean
+     */
     private $loggedIn = false;
-    
-   /**
-    * @var array<string,string>
-    */
+
+    /**
+     * @var array<string,string>
+     */
     protected $pages = array(
         'list' => 'Home',
         'add' => 'Add new',
@@ -34,141 +40,169 @@ class Manager
         'login' => 'Login'
     );
 
-   /**
-    * @var string
-    */
+    /**
+     * @var string
+     */
     protected $version;
-    
+
     public static function getInstance() : Manager
     {
-        if(!isset(self::$instance)) {
+        if (!isset(self::$instance))
+        {
             self::$instance = new Manager();
         }
-        
+
         return self::$instance;
     }
-    
+
     protected function __construct()
     {
-        $this->series = new Series();
-        $this->version = file_get_contents(APP_ROOT.'/version.txt');
-        
+        $this->series = new SeriesCollection();
+        $this->version = file_get_contents(APP_ROOT . '/version.txt');
+
         session_start();
     }
-    
+
     public function getVersion() : string
     {
         return $this->version;
     }
-    
+
     public function getPages() : array
     {
         return $this->pages;
     }
-    
+
     public function getPageID() : string
     {
         return $this->page;
     }
-    
+
     public function start() : void
     {
-        $page = $this->page;
-        if(isset($_REQUEST['page'])) {
-            $page = $_REQUEST['page'];
-        }
+        $page = $_REQUEST['page'] ?? $this->page;
 
-        if(!$this->checkLogin())
+        if (!$this->checkLogin())
         {
             $page = 'login';
         }
-        
-        if(isset($this->pages[$page])) {
+
+        if (isset($this->pages[$page]))
+        {
             $this->page = $page;
         }
-        
-        if($page=='delete' && $this->getSelectedID()) {
+
+        if ($page === 'delete' && $this->getSelectedID())
+        {
             $this->page = 'delete';
         }
-        
-        if($page=='edit' && $this->getSelectedID()) {
+
+        if ($page === 'edit' && $this->getSelectedID())
+        {
             $this->page = 'edit';
         }
-        
+
         ob_start();
-        require APP_ROOT.'/pages/'.$this->page.'.php';
+        require APP_ROOT . '/pages/' . $this->page . '.php';
         $this->content = ob_get_clean();
-        
-        require APP_ROOT.'/pages/_frame.php';
+
+        require APP_ROOT . '/pages/_frame.php';
     }
-    
-   /**
-    * @var string
-    */
+
+    /**
+     * @var string
+     */
     protected $content = '';
-    
+
     public function getContent() : string
     {
         return $this->content;
     }
-    
-   /**
-    * @return Series
-    */
-    public function getSeries() : Series
+
+    /**
+     * @return SeriesCollection
+     */
+    public function getSeries() : SeriesCollection
     {
         return $this->series;
     }
-    
+
     public function getSelectedID() : string
     {
-        if(isset($_REQUEST['id']) && $this->series->rarbgIDExists($_REQUEST['id'])) {
+        if (isset($_REQUEST['id']) && $this->series->IMDBIDExists($_REQUEST['id']))
+        {
             return $_REQUEST['id'];
         }
-        
+
         return '';
     }
-    
+
     public function getSelected() : ?Series
     {
         $id = $this->getSelectedID();
-        if($id) {
-            return $this->series->getByRarbgID($id);
+        if ($id)
+        {
+            return $this->series->getByIMDBID($id);
         }
-        
+
         return null;
     }
-    
+
     private function checkLogin() : bool
     {
-        if(!isset($_SESSION['auth']))
+        if (!isset($_SESSION['auth']))
         {
             return false;
         }
-        
+
         $authKey = $this->encodePassword(APP_PASSWORD);
-        
-        if($_SESSION['auth'] === $authKey)
+
+        if ($_SESSION['auth'] === $authKey)
         {
             $this->loggedIn = true;
             return true;
         }
-        
+
         return false;
     }
-    
+
     public function isLoggedIn() : bool
     {
         return $this->loggedIn;
     }
-    
+
     public function isPasswordValid(string $password) : bool
     {
         return $password === APP_PASSWORD;
     }
-    
+
     public function encodePassword(string $password) : string
     {
-        return sha1($password.'-'.APP_SALT);
+        return sha1($password . '-' . APP_SALT);
+    }
+
+    private ?Client $client = null;
+
+    public function createClient() : Client
+    {
+        if (isset($this->client))
+        {
+            return $this->client;
+        }
+
+        $client = new Client();
+        $client->setLanguage('en');
+
+        $token = $client->authentication()->login(
+            APP_API_KEY,
+            null,
+            APP_SUBSCRIBER_PIN
+        );
+
+        $client->setToken($token);
+
+        $this->client = $client;
+
+        return $client;
     }
 }
