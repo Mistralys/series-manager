5951588b08b140b6fea20c340375dae75accbba9
Mistralys <s.mordziol@gmail.com>
2023-07-19 16:34:53 +0200

    Updated HTML structure;  Added completed status.

--- pages/list.php
+++ pages/list.php
@@ -4,7 +4,11 @@ declare(strict_types=1);
 
 namespace Mistralys\SeriesManager\Pages;
 
+use AppUtils\JSHelper;
 use Mistralys\SeriesManager\Manager;
+use Mistralys\SeriesManager\UI;
+use function AppLocalize\pt;
+use function AppLocalize\ptex;
 
 $manager = Manager::getInstance();
 $series = $manager->getSeries();
@@ -23,42 +27,46 @@ if(isset($_REQUEST['update'], $_REQUEST['series']) && $_REQUEST['update'] === 'y
 
 $items = $series->getAll();
 
-$activeID = $manager->getSelectedID();
-
-$html = '';
 if(empty($items)) {
-    echo
-    '<div class="alert alert-info">'.
-        'No series found.'.
-    '</div>';
-    
+    ?>
+    <div class="alert alert-info">
+        <?php pt('No series found.') ?>
+    </div>
+    <?php
     return;
 } 
 
-$html .=
-'<h3>Available series</h3>'.
-'<form method="post" class="form-inline">'.
-    '<table class="table table-hover">'.
-        '<thead>'.
-            '<tr>'.
-                '<td>Name</td>'.
-                '<td>Status</td>'.
-                '<td style="text-align: right">Seasons</td>'.
-                '<td style="text-align: right">Episodes</td>'.
-                '<td>Links</td>'.
-                '<td>Last Downloaded</td>'.
-                '<td></td>'.
-            '</tr>'.
-        '</thead>'.
-        '<tbody>';
-            foreach($items as $item) {
-                $html .=
-                '<tr>'.
-                    '<td><a href="?id='.$item->getIMDBID().'">'.$item->getName().'</a></td>'.
-                    '<td>'.$item->getStatus().'</td>'.
-                    '<td style="text-align: right">'.$item->countSeasons().'</td>'.
-                    '<td style="text-align: right">'.$item->countEpisodes().'</td>'.
-                    '<td>';
+?>
+<h3><?php pt('Available series') ?></h3>
+<form method="post" class="form-inline">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <td><?php pt('Name') ?></td>
+                <td><?php pt('Status') ?></td>
+                <td style="text-align: right"><?php pt('Seasons') ?></td>
+                <td style="text-align: right"><?php pt('Episodes') ?></td>
+                <td style="text-align: center"><?php pt('Complete?') ?></td>
+                <td><?php pt('Links') ?></td>
+                <td><?php pt('Last Downloaded') ?></td>
+                <td></td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach($items as $item)
+            {
+                $rowID = JSHelper::nextElementID();
+
+                ?>
+                <tr>
+                    <td><a href="<?php echo $item->getURLEdit() ?>"><?php echo $item->getName() ?></a></td>
+                    <td><?php echo $item->getStatus() ?></td>
+                    <td style="text-align: right"><?php echo $item->countSeasons() ?></td>
+                    <td style="text-align: right"><?php echo $item->countEpisodes() ?></td>
+                    <td style="text-align: center"><?php echo UI::prettyBool($item->isComplete()) ?></td>
+                    <td>
+                        <?php
                         $links = $item->getLinks();
                         $tokens = array();
                         foreach($links as $link) {
@@ -68,54 +76,54 @@ $html .=
                             '</a>';
                         }
                         
-                        $html .= 
-                        implode(' | ', $tokens).
-                    '</td>'.
-                    '<td>'.
-                        '<div class="form-group">'.
-                            '<div class="input-group">'.
-                                '<div class="input-group-addon">'.
-                                    'S'.
-                                '</div>'.
-                                '<input name="series['.$item->getIMDBID().'][lastDLSeason]" type="number" class="form-control" value="'.$item->getLastDLSeason().'" style="width:60px"/>'.
-                            '</div> '.
-                            '<div class="input-group">'.
-                                '<div class="input-group-addon">'.
-                                    'E'.
-                                '</div>'.
-                                '<input name="series['.$item->getIMDBID().'][lastDLEpisode]" type="number" class="form-control" value="'.$item->getLastDLEpisode().'" style="width:60px"/>'.
-                            '</div>'.
-                        '</div>'.
-                    '</td>'.
-                    '<td>'.
-                        '<a href="'.$item->getURLEdit().'" class="btn btn-default">'.
-                            '<i class="glyphicon glyphicon-edit"></i> '.
-                        '</a> '.
-                        '<a href="'.$item->getURLDelete().'" class="btn btn-danger">'.
-                            '<i class="glyphicon glyphicon-remove-sign"></i> '.
-                        '</a>'.
-                    '</td>'.
-                '</tr>';
-        
-                if($activeID===$item->getIMDBID()) {
-                    $html .=
-                    '<tr>'.
-                        '<td colspan="7">'.
-                            '<iframe style="width:100%;height:500px;border:0;margin:0;padding:0;" src="'.$item->getRarbgLink().'" seamless>'.
-                            '</iframe>'.
-                        '</td>'.
-                    '</tr>';
-                }
+                        echo implode(' | ', $tokens);
+                        ?>
+                    </td>
+                    <td>
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-addon">
+                                    <label for="<?php echo $rowID ?>-lastDLSeason">
+                                        <?php ptex('S', 'Single letter used to represent a `Season`.') ?>
+                                    </label>
+                                </div>
+                                <input  name="series[<?php echo $item->getIMDBID() ?>][lastDLSeason]"
+                                        id="<?php echo $rowID ?>-lastDLSeason"
+                                        type="number"
+                                        class="form-control"
+                                        value="<?php echo $item->getLastDLSeason() ?>"
+                                        style="width:60px"/>
+                            </div>
+                            <div class="input-group">
+                                <div class="input-group-addon">
+                                    <label for="<?php echo $rowID ?>-lastDLEpisode">
+                                        <?php ptex('E', 'Single letter used to represent an `Episode`.') ?>
+                                    </label>
+                                </div>
+                                <input  name="series[<?php echo $item->getIMDBID() ?>][lastDLEpisode]"
+                                        id="<?php echo $rowID ?>-lastDLEpisode"
+                                        type="number"
+                                        class="form-control"
+                                        value="<?php echo $item->getLastDLEpisode() ?>"
+                                        style="width:60px"/>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        <a href="<?php echo $item->getURLDelete() ?>" class="btn btn-danger">
+                            <i class="glyphicon glyphicon-remove-sign"></i>
+                        </a>
+                    </td>
+                </tr>
+                <?php
             }
-            $html .=
-        '</tbody>'.
-    '</table>'.
-    '<p>'.
-        '<button name="update" type="submit" class="btn btn-primary" value="yes">'.
-            '<i class="glyphicon glyphicon-edit"></i> '.
-            'Update'.
-        '</button>'.
-    '</p>'.
-'</form>';
-
-echo $html;
+            ?>
+        </tbody>
+    </table>
+    <p>
+        <button name="update" type="submit" class="btn btn-primary" value="yes">
+            <i class="glyphicon glyphicon-edit"></i>
+            <?php pt('Save positions') ?>
+        </button>
+    </p>
+</form>
