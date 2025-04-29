3352b49b41c5e08c6ab251d2118ffaa5f0e67492
Mistralys <s.mordziol@gmail.com>
2025-04-29 20:10:04 +0200

    Custom page title handling.

--- pages/_frame.php
+++ pages/_frame.php
@@ -15,7 +15,7 @@ $manager = Manager::getInstance();
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo Manager::getName() ?></title>
+    <title><?php echo Manager::getDocumentTitle() ?></title>
     <?php
     if(Manager::isDarkMode())
     {
